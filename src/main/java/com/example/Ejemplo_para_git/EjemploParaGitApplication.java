d8ac5497752170446d245329b8843d7e44ef2b01
FrankSarabia <francisco290900@gmail.com>
2024-05-28 16:09:58 -0600

    feat: inicio del proyecto

---
+++ src/main/java/com/example/Ejemplo_para_git/EjemploParaGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ejemplo_para_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploParaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploParaGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ejemplo_para_git/EjemploParaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ejemplo_para_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploParaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
